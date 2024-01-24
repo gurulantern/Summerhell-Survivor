16c8ed263fbc28e424a5c711c397a869265d1338
Alex Ho <87953031+gurulantern@users.noreply.github.com>
2024-01-23 21:59:03 -0500

    Started work on Interiors for other restaurants
    Added Heesoo-64
    Added interior tilemaps for Crispee, Tart, Softie, and Chapel

---
+++ Player/Heesoo_64.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+
+const speed = 80
+@onready var anim = $AnimatedSprite2D
+@onready var collision = $CollisionShape2D
+
+
+func _physics_process(delta):
+	var direction : Vector2 = Input.get_vector("left", "right", "up", "down").normalized()
+
+	if direction: 
+		velocity = direction * speed
+	else:
+		velocity = Vector2.ZERO
+	
+	if velocity.x > 0:
+		anim.flip_h = true
+	elif velocity.x < 0:
+		anim.flip_h = false
+	
+	if velocity.y != 0 or velocity.x != 0:
+		anim.play("walk")
+	else:
+		anim.play("idle")
+	
+	move_and_slide()
